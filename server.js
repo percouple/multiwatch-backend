512435152d01fccd24be69348b0324da503f6612
percouple <calebsevan@gmail.com>
2024-12-30 16:46:29 -0600

    gitignore commit

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
+import config from 'dotenv';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     console.log("Request happening")
-    res.send("get request working")
+    res.status(404)
+    res.json({ message: "Heyo json working my man"})
 })
 
 app.listen(port, () => {
