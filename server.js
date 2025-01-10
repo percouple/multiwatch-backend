eda35972dc7aa81f5182bc41ff5aed69d2e11718
percouple <calebsevan@gmail.com>
2025-01-10 15:22:38 -0600

    lib movement from frontend

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 import express from "express";
-import config from 'dotenv';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     console.log("Request happening")
-    res.status(404)
+    res.status(200)
     res.json({ message: "Heyo json working my man"})
 })
 
 app.listen(port, () => {
-    console.log(`Server runing on port ${port}`)
+    console.log(`Server running on port ${port}`)
 })
