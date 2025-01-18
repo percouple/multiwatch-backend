39c891ff4e7582f9558d3fd156630a1add3c0941
Caleb Evans <calebsevan@gmail.com>
2025-01-17 21:10:27 -0600

    scaffold more https requests

--- server.js
+++ server.js
@@ -7,12 +7,28 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json())
 
-app.get('/', async (req, res) => {
+app.get('/get-user', async (req, res, next) => {
     try {
         const user = await findUser(req.body.username);
         res.status(200).json(user)
     }
     catch {
+        res.status(400).json({ message: "Heyo it broke"})
+    }
+})
+
+app.post('/create-clock', async (req, res) => {
+    try {
+        res.status(200).json()
+    } catch {
+        res.status(400).json({ message: "Heyo it broke"})
+    }
+})
+
+app.post('/create-user', async (req, res) => {
+    try {
+        res.status(200).json()
+    } catch {
         res.status(400).json({ message: "Heyo it broke"})
     }
 })
